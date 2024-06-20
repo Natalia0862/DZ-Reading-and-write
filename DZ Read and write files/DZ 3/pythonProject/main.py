2d7039eb041f09a7e56eab74d06a22c50f9491e6
Natalia0862 <natalabarabanova0862@gmail.com>
2024-06-20 17:53:51 +0300

    Add files via upload

--- DZ Read and write files/DZ 3/pythonProject/main.py
+++ DZ Read and write files/DZ 3/pythonProject/main.py
@@ -1,13 +1,14 @@
 import os
-import pprint
+import operator
+
 path = r"C:\Users\New\Desktop\Netology\DZ Read and write files\DZ 3\pythonProject\5.txt"
-path_2 = r"C:\Users\New\Desktop\Netology\DZ Read and write files\DZ 3\pythonProject\4.txt"
 
 list_files = [x for x in os.listdir(r'C:\Users\New\Desktop\Netology\DZ Read and write files\DZ 3') if x == '1.txt' or x == '2.txt' or x == '3.txt']
-new_dict = dict()
-print(list_files)
 
+new_dict = dict()
+sorted_dict = dict()
 count = int(0)
+
 for file in list_files:
     def strings_count(file):
        with (open(os.path.join(r'C:\Users\New\Desktop\Netology\DZ Read and write files\DZ 3', file), 'r') as f):
@@ -17,18 +18,15 @@ for file in list_files:
         count = strings_count(file)
         new_dict[file] = count, list_news
 
-pprint.pprint(new_dict)
+
+sorted_dict = sorted(new_dict.items(), key=operator.itemgetter(1))
+new_dict = dict(sorted_dict)
 
 for item in new_dict:
     opening_files = open(path, 'a', encoding= 'utf-8')
     opening_files.write(f'Количество строк {new_dict[item]}\n')
     opening_files.write(f'\n')
     opening_files.close()
-'''        with open(path, 'r', encoding= 'utf-8') as q:
-            count = int(1)
-            for line in q:
-                opening_files.write(f'строк {count} в файле {new_dict} : {line}')
-                count += 1
-                opening_files.write(f'\n')'''
+
 
 
